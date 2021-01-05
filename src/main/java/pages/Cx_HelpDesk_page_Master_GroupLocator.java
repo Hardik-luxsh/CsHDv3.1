056ee3b408b2d4ef3471150ad9e797bb9b9f5096
autom99 <ehardik92@gmail.com>
2021-01-05 19:13:15 +0530

    Master-Employee Module-Create,Edit & Delete on 05-Jan-2021.

--- src/main/java/pages/Cx_HelpDesk_page_Master_GroupLocator.java
+++ src/main/java/pages/Cx_HelpDesk_page_Master_GroupLocator.java
@@ -1,5 +1,6 @@
 package pages;
 
+import TestUtil.CheckBox;
 import TestUtil.GenericUtil;
 import TestUtil.HighlightElement;
 import base.TestBase;
@@ -16,65 +17,68 @@ public class Cx_HelpDesk_page_Master_GroupLocator extends TestBase{
 
     public static WebDriverWait wait;
     public static GenericUtil genericUtil;
+    public static CheckBox objCheckBox;
 
+    /*--------------Group ---------------*/
     @FindBy(xpath = "//h1[@class='page-title']")
-    WebElement TitleMasters;
+    public WebElement TitleMasters;
 
-    @FindBy(xpath = "//span[text()=' Group Master']")
-    WebElement TitleGroup;
+    @FindBy(xpath = "(//span[@class='caption-subject font-dark bold uppercase'])[2]")
+    public WebElement TitleGroup;
 
     @FindBy(xpath = "//i[@class='fa fa-database']")
     public WebElement sidebar_textMaster;
 
     @FindBy(xpath = "(//a[@class='collapse btn btn-circle btn-icon-only btn-default'])[2]")
-    WebElement GroupCollapse;
+    public WebElement GroupCollapse;
 
     @FindBy(xpath = "(//a[@class='btn btn-circle btn-icon-only btn-default fullscreen'])[2]")
-    WebElement GroupFullScreen;
+    public WebElement GroupFullScreen;
 
     @FindBy(xpath ="(//select[@class='fontColor'])[2]")
-    WebElement GroupList;
+    public WebElement GroupList;
 
     @FindBy(xpath = "(//input[@class='fontColor'])[2]")
-    WebElement GroupSearch;
+    public WebElement GroupSearch;
 
     @FindBy(xpath = "(//tr[@class='odd shown'])[1]")
-    WebElement GroupData;
+    public WebElement GroupData;
 
     @FindBy(xpath = "(//tr/td[@class='text-center btnPlus'])[1]")
-    WebElement GroupBtnPlus;
+    public WebElement GroupBtnPlus;
 
     @FindBy(xpath = "(//tr/td[@class='text-center btnMinus'])[1]")
-    WebElement GroupBtnMinus;
+    public WebElement GroupBtnMinus;
 
     @FindBy(xpath = "(//tr[@class='ng-star-inserted'])[3]")
-    WebElement GroupDataListFirstRow;
+    public WebElement GroupDataListFirstRow;
 
     @FindBy(xpath = "(//input[@class='fontColor'])[3]")
-    WebElement GroupDataSearch;
+    public WebElement GroupDataSearch;
 
     @FindBy(xpath = "(//tr[@class='ng-star-inserted'])[3]")
-    WebElement GroupEmpDataFirstRow;
+    public WebElement GroupEmpDataFirstRow;
 
     @FindBy(xpath = "(//span[@class='ng-star-inserted'])[3]")
-    WebElement GroupEmpPaginationPrevious;
+    public WebElement GroupEmpPaginationPrevious;
 
     @FindBy(xpath = "(//span[@class='ng-star-inserted'])[5]")
-    WebElement GroupEmpPaginationNext;
+    public WebElement GroupEmpPaginationNext;
 
     @FindBy(xpath = "(//span[@class='ng-star-inserted'])[4]")
-    WebElement GroupEmpPaginationNo ;
+    public WebElement GroupEmpPaginationNo ;
 
-    /*--------------Edit---------------*/
+    /*--------------Edit Group ---------------*/
 
     @FindBy(xpath = "(//i[@title='Edit Group'])[1]")
-    WebElement GroupEditBtn;
+    public WebElement GroupEditBtn;
 
     @FindBy(xpath = "((//i[@title='Delete Group'])[1]")
-    WebElement GroupDeleteBtn;
+    public WebElement GroupDeleteBtn;
 
     @FindBy(xpath = "//input[@class='form-control ng-pristine ng-valid ng-touched']")
-    WebElement GroupEditName;
+    public WebElement GroupEditName;
+
     /*
 
     //don't no which xpath use
@@ -83,52 +87,68 @@ public class Cx_HelpDesk_page_Master_GroupLocator extends TestBase{
     WebElement GroupEditAttachedUser;
 
     */
+
     @FindBy(xpath ="(//label[@class='mt-checkbox mt-checkbox-single mt-checkbox-outline']/span)[2]")
-    WebElement GroupEditStatus;
+    public WebElement GroupEditStatus;
 
     @FindBy(xpath = "(//input[@class='btn btn-success'])[5]")
-    WebElement GroupEditSubmit;
+    public WebElement GroupEditSubmit;
 
     @FindBy(xpath = "(//input[@class='btn btn-success'])[6]")
-    WebElement GroupEditClear;
+    public WebElement GroupEditClear;
 
     @FindBy(xpath = "(//input[@class='btn btn-success'])[7]")
-    WebElement GroupEditCancel;
+    public WebElement GroupEditCancel;
 
     @FindBy(xpath = "(//a[@class='btn btn-success language_btn'])[3]")
-    WebElement GroupAdd;
+    public WebElement GroupAdd;
 
     @FindBy(xpath = "(//a[@class='btn btn-success language_btn'])[4]")
-    WebElement GroupImport;
+    public WebElement GroupImport;
+
+    @FindBy(xpath = "//h3[contains(text(),'Are you sure! You want to Delete this Group?')]")
+    public WebElement DeleteGroupMessage;
+
+    @FindBy(xpath = "(//button[@class='btn btn-lg btn-success'])[2]")
+    public WebElement YesOptionDeleteGroupMessage;
+
 
     /*--------------Add New Group ---------------*/
-
     @FindBy(xpath = "//input[@id='Name']")
-    WebElement GroupAddName;
+    public WebElement GroupAddName;
 
     @FindBy(xpath = "(//div[@class='cuppa-dropdown'])[1]")
-    WebElement GroupAddAttachedUser;
+    public WebElement GroupAddAttachedUser;
+
+    @FindBy(xpath = "//label[@for='selectAll']")
+    public WebElement SelectedAllAttachedUser;
+
+    @FindBy(xpath = "//span[contains(text(),'Deselect All')]")
+    public WebElement DeSelectedAllAttachedUser;
+
+    @FindBy(xpath = "(//span[@class='c-angle-down ng-star-inserted'])[1]")
+    public WebElement downArrowAttachedUser;
 
     @FindBy(xpath = "(//label[@class='mt-checkbox mt-checkbox-single mt-checkbox-outline']/span)[2]")
-    WebElement GroupAddStatus;
+    public WebElement GroupAddStatus;
 
     @FindBy(xpath = "(//input[@class='btn btn-success'])[5]")
-    WebElement GroupAddSubmit;
+    public WebElement GroupAddSubmit;
 
     @FindBy(xpath = "(//input[@class='btn btn-success'])[6]")
-    WebElement GroupAddClear;
+    public WebElement GroupAddClear;
 
     @FindBy(xpath = "(//input[@class='btn btn-success'])[7]")
-    WebElement GroupAddCancel;
+    public WebElement GroupAddCancel;
 
     @FindBy(xpath = "(//a[@class='ng-star-inserted'])[8]")
-    WebElement GroupPaginationPrevious;
+    public WebElement GroupPaginationPrevious;
 
     @FindBy(xpath = "(//a[@class='ng-star-inserted'])[11]")
-    WebElement GroupPaginationNext;
+    public WebElement GroupPaginationNext;
 
     @FindBy(xpath = "(//a[@class='ng-star-inserted'])[10]")
-    WebElement GroupPaginationNo ;
+    public WebElement GroupPaginationNo ;
 
     public Cx_HelpDesk_page_Master_GroupLocator(WebDriver driver) {
         TestBase.driver = driver;
@@ -136,9 +156,10 @@ public class Cx_HelpDesk_page_Master_GroupLocator extends TestBase{
         wait = new WebDriverWait(driver, 20);
     }
 
-    public void createGroup(){
-        genericUtil = new GenericUtil();
+    public void createGroup(String GroupName,String searchText){
         try {
+            genericUtil = new GenericUtil();
+
             wait.until(ExpectedConditions.visibilityOf(sidebar_textMaster));
             HighlightElement.highlightElement(sidebar_textMaster);
             sidebar_textMaster.click();
@@ -156,19 +177,94 @@ public class Cx_HelpDesk_page_Master_GroupLocator extends TestBase{
             HighlightElement.highlightElement(GroupAdd);
             genericUtil.clickWithPause(GroupAdd,3000);
 
-            wait.until(ExpectedConditions.visibilityOf(GroupAddName));
-//            Actions builder = new Actions(driver);
-//            builder.moveToElement(GroupAddName).click().build().perform();
-
             HighlightElement.highlightElement(GroupAddName);
-            genericUtil.writeTextWithPause(GroupAddName,"Group Test",2000);
+            genericUtil.writeTextWithPause(GroupAddName,GroupName,2000); //"Group Test"
 
-            GroupAddAttachedUser.click();
+            HighlightElement.highlightElement(GroupAddAttachedUser);
+            genericUtil.clickWithPause(GroupAddAttachedUser,1000);
 
+            HighlightElement.highlightElement(SelectedAllAttachedUser);
+            genericUtil.clickWithPause(SelectedAllAttachedUser,1000);
+
+            HighlightElement.highlightElement(downArrowAttachedUser);
+            genericUtil.clickWithPause(downArrowAttachedUser,1000);
+
+            try {
+                objCheckBox = new CheckBox();
+                HighlightElement.highlightElement(GroupAddStatus);
+                objCheckBox.Select_The_Checkbox(GroupAddStatus);
+                genericUtil.pause(1000);
+                HighlightElement.highlightElement(GroupAddStatus);
+                objCheckBox.DeSelect_The_Checkbox(GroupAddStatus);
+                genericUtil.pause(1000);
+                HighlightElement.highlightElement(GroupAddStatus);
+                objCheckBox.Select_The_Checkbox(GroupAddStatus);
+                genericUtil.pause(1000);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            HighlightElement.highlightElement(GroupAddSubmit);
+            genericUtil.clickWithPause(GroupAddSubmit,3000);
+
+            HighlightElement.highlightElement(GroupSearch);
+            genericUtil.writeTextWithPause(GroupSearch,searchText,3000); //"Group Test"
         }
         catch (Exception ex){
             ex.getStackTrace();
         }
     }
 
+    public void editGroup(String UpdatedGroupName,String searchText){
+        try {
+            genericUtil = new GenericUtil();
+
+            HighlightElement.highlightElement(GroupDataListFirstRow);
+            genericUtil.writeTextWithPause(GroupDataListFirstRow,"",3000); //"Group Test"
+
+            HighlightElement.highlightElement(GroupEditBtn);
+            genericUtil.clickWithPause(GroupEditBtn,3000);
+
+            HighlightElement.highlightElement(GroupAddName);
+            genericUtil.writeTextWithPause(GroupAddName,UpdatedGroupName,2000); //"Group Test"
+
+            HighlightElement.highlightElement(GroupAddAttachedUser);
+            genericUtil.clickWithPause(GroupAddAttachedUser,1000);
+
+            HighlightElement.highlightElement(DeSelectedAllAttachedUser);
+            genericUtil.clickWithPause(DeSelectedAllAttachedUser,1000);
+
+            HighlightElement.highlightElement(downArrowAttachedUser);
+            genericUtil.clickWithPause(downArrowAttachedUser,1000);
+
+            HighlightElement.highlightElement(GroupAddSubmit);
+            genericUtil.clickWithPause(GroupAddSubmit,3000);
+
+            HighlightElement.highlightElement(GroupSearch);
+            GroupSearch.clear();
+            genericUtil.writeTextWithPause(GroupSearch,searchText,3000); //"Group Test"
+        }
+        catch (Exception ex){
+            ex.getStackTrace();
+        }
+    }
+
+    public void deleteGroup(String searchText){
+        try {
+            genericUtil = new GenericUtil();
+            HighlightElement.highlightElement(GroupDeleteBtn);
+            genericUtil.clickWithPause(GroupDeleteBtn, 3000);
+
+            HighlightElement.highlightElement(DeleteGroupMessage);
+
+            HighlightElement.highlightElement(YesOptionDeleteGroupMessage);
+            genericUtil.clickWithPause(YesOptionDeleteGroupMessage, 3000);
+
+            HighlightElement.highlightElement(GroupSearch);
+            GroupSearch.clear();
+            genericUtil.writeTextWithPause(GroupSearch, searchText, 3000); //"Group Test"
+        }catch (Exception ex){
+            ex.getStackTrace();
+        }
+    }
 }
--- src/main/java/testcase/cxHelpDesk/CxHelpDesk_Master_GroupPageTests.java
+++ src/main/java/testcase/cxHelpDesk/CxHelpDesk_Master_GroupPageTests.java
@@ -23,11 +23,33 @@ public class CxHelpDesk_Master_GroupPageTests extends TestBase {
         objLoginPage.validateLogin(Constants.ADMINUSERNAME, Constants.VALIDPASSWORD);
     }
 
-    @Test
-    public void createEmployee(){
+    @Test(priority = 1)
+    public void createGroup(){
         try {
             objMasterGroupLocator = new Cx_HelpDesk_page_Master_GroupLocator(driver);
-            objMasterGroupLocator.createGroup();
+            objMasterGroupLocator.createGroup("Group Name","Group Name");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Test(priority = 2)
+    public void editGroup(){
+        try {
+            objMasterGroupLocator = new Cx_HelpDesk_page_Master_GroupLocator(driver);
+            objMasterGroupLocator.editGroup("Updated Group Name","Updated Group Name");
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+    @Test(priority = 3)
+    public void deleteGroup(){
+        try {
+            objMasterGroupLocator = new Cx_HelpDesk_page_Master_GroupLocator(driver);
+            objMasterGroupLocator.deleteGroup("Updated Group Name");
         }
         catch (Exception ex){
             ex.printStackTrace();
